0645cd65b2d50acf26c1eac9d55edcd35ef002d6
johnwav <102820657+johnwav@users.noreply.github.com>
2023-03-04 00:17:34 +0100

    Feat: Filter component funtionality

--- src/Components/Filter.component/index.jsx
+++ src/Components/Filter.component/index.jsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
 import styles from "./Filter.module.css";
 
-export const Filter = () => {
-  const [filter, setFilter] = useState("All");
-
-  const handleChange = (e) => {
-    setFilter(e.target.value);
+export const Filter = (props) => {
+  const onFilterChange = (e) => {
+    props.filterValueSelected(e.target.value);
   };
-
   return (
     <>
-      <select
-        name="Filter by Region"
-        className={styles.dropdown}
-        onChange={handleChange}
-      >
-        {console.log(filter)}
-
-        
+      <select className={styles.dropdown} onChange={onFilterChange}>
         <option value={"All"}>Filter by Region</option>
         <option value={"Africa"}>Africa</option>
         <option value={"America"}>America</option>
